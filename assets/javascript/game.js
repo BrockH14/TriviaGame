1fc0f949f1e96dbd55b4b78dde578e47a5102152
BrockH <brock.horton@gmail.com>
2020-03-03 18:26:16 -0500

    adding base javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+// variables 
+var answersA = [];
+var answersB = [];
+var answersC = [];
+var answersD = [];
+var correctAns = [];
+
+var inter;
+
+var runtime = false;
+var timer = 30;
+
+var guess;
+//set up 4 arrays to hold the answers 
+//set up one array to hold the correct answers
+
+//on click start function 
+$("start").on("click", function(){
+//create timer 
+ setTimeout(timer, 30000)
+//per question
+for ()
+function start() {
+
+    if (!Runtime) {
+      inter = setInterval(count, 1000);
+      clockRunning = true;
+    }
+  }
+
+
+  function count() {
+    time--;
+    var converted = timeConverter(time);
+    console.log(converted);
+    $("#display").text(converted);
+  }
+
+
+
+
+
+    //write out the 4 answers 
+
+    //conditionals to see if they chose the right answer or the wrong answer]
+
+//display correct or incorrect or out of time
+//display the correct answer 
+//maybe add photo and sound??
+
+//go to the next question 
+
+
+//closing prompt 
+
+//number of correct answers
+//number of incorrect answers
+//number of unanswered 
+
+//start over?
+
+});
+
+
