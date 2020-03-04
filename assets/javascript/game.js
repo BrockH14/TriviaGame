8536b1ccb61bd5329e8c9fe60b1775719eadd8a3
BrockH <brock.horton@gmail.com>
2020-03-03 19:35:47 -0500

    time ran out function working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,36 +8,55 @@ var correctAns = [];
 var inter;
 
 var runtime = false;
-var timer = 30;
+var timer = 10;
+
+var wins
+var losses = 0;
 
 var guess;
 //set up 4 arrays to hold the answers 
 //set up one array to hold the correct answers
-
+//window.onload = function() {
+   // $("#start").on("click", start);
+ // };
 //on click start function 
-$("start").on("click", function(){
+$("#start").on("click", function(){
 //create timer 
  setTimeout(timer, 30000)
 //per question
-for ()
+
 function start() {
 
-    if (!Runtime) {
+    if (!runtime) {
       inter = setInterval(count, 1000);
       clockRunning = true;
+      document.getElementById("start").style.visibility = "hidden";
     }
   }
 
 
   function count() {
-    time--;
-    var converted = timeConverter(time);
-    console.log(converted);
-    $("#display").text(converted);
+      if (timer >= 1){
+        timer--;
+        $("#Timer").text(timer);
+      } else if (timer === 0){
+          losses++;
+          setTimeout(restart, 5000);
+          $("#Losses").text(losses);
+          timer = 15;
+          document.getElementById("Timer").style.visibility = "hidden";
+
+      }
   }
+function restart(){
+    clockRunning = false;
+    timer = 10;
+    console.log(clockRunning);
+    document.getElementById("Timer").style.visibility = "visible";
+}
 
 
-
+start();
 
 
     //write out the 4 answers 
